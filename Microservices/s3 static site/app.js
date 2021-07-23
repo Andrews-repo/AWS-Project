b1d01e4ee30decd67ee4786d491ffaa6cc223230
Andrew <59451276+Andrews-repo@users.noreply.github.com>
2021-07-23 05:25:46 -0700

    Add files via upload

---
+++ Microservices/s3 static site/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $.ajax({
+    type: "POST",
+    data: 'json',
+    contentType: 'json',
+    url: 'https://api.apcloudtech.com/get',
+    success: function(result) {
+      console.log(result)
+      $( ".fortune" ).text(result);
+    },
+    error: function(result) {
+      console.log('error', result)
+      $(".error").text(result.error)
+    }
+  });                              
+  
+  
+  
+  $("form").submit(function(e) {
+      console.log(e)
+      console.log("form serialized", $('form').serialize());
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        data: $('form').serialize(),
+        dataType: 'json',
+        url: 'https://api.apcloudtech.com/',
+        success: function(result) {
+          console.log(result)
+          $( ".fortune" ).text(result);
+        },
+        error: function(result) {
+          console.log('error', result)
+          $(".error").text(result.error)
+        }
+      });                                                                                               
+    });
+});
